9981412b12c35ac6c21a3b55b5940eb80d1a71b8
Ekctiandro_Goncalo <ekctiandrog@gmail.com>
2024-10-04 09:24:45 +0100

    Add components folder

---
+++ components/pt.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Pt = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Pt;
